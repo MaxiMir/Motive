1974cbffc7fd1daf98199d91bb071197a6af9315
Maxim <mmirrev@gmail.com>
2021-08-20 23:18:00 +0300

    Creating pages

--- route.ts
+++ route.ts
@@ -6,12 +6,14 @@ export default {
   FAVORITES: '/favorites',
   FAVORITES_USERS: '/favorites/user',
   getFavoriteUserId(id: string): string {
-    return [this.FAVORITES_USERS, id].join('/')
+    return toUrn(this.FAVORITES_USERS, id)
   },
   PROFILE: '/profile',
   SIGN_IN: '/sign-in',
   TASK: '/task',
   getTaskId(id: string): string {
-    return [this.TASK, id].join('/')
+    return toUrn(this.TASK, id)
   },
 }
+
+const toUrn = (...parts: string[]) => parts.join('/')

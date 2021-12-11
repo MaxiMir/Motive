f9eec49d1be3d34f340a6ad4f833d72f91a4b56d
Maxim <mmirrev@gmail.com>
2021-12-11 23:25:24 +0300

    feat(dev): creating pages

--- components/UserCard/tmpl/UserCardDetail/components/AddGoal/components/Modal/helper.ts
+++ components/UserCard/tmpl/UserCardDetail/components/AddGoal/components/Modal/helper.ts
@@ -3,7 +3,7 @@ import { GoalCreation } from 'dto'
 
 export const schema: SchemaOf<GoalCreation> = object({
   name: string().trim().required('Goal name needed').min(5, "It's too short.").max(32, "It's so long."),
-  hashtags: string().trim().max(150, "It's so long."),
+  hashtags: string().trim().max(255, "It's so long."),
   tasks: array().of(
     object({
       name: string().trim().required('Task content needed').min(5, "It's too short.").max(255, "It's too long."),

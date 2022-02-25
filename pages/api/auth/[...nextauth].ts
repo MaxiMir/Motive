23787333888724391de3d864c821895f3f7b0e0d
Maxim <mmirrev@gmail.com>
2022-02-25 23:24:54 +0300

    feat(main-page): creating pages

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth'
 import AppleProvider from 'next-auth/providers/apple'
 import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
-import { decode } from 'next-auth/jwt'
 
 export default NextAuth({
   providers: [
@@ -25,30 +24,5 @@ export default NextAuth({
   },
   pages: {
     error: '/500',
-  },
-  callbacks: {
-    async signIn({ user, account, profile }) {
-      console.log(user, account, profile)
-
-      return true
-    },
-    async redirect({ url }) {
-      // todo redirect to profile edit
-      return url
-    },
-  },
-  jwt: {
-    // The maximum age of the NextAuth.js issued JWT in seconds.
-    // Defaults to `session.maxAge`.
-    maxAge: 60 * 60 * 24 * 30,
-    // You can define your own encode/decode functions for signing and encryption
-    // async encode({ token }) {
-    //
-    // },
-    async decode({ token, secret }) {
-      const d = await decode({ token, secret })
-      console.log(d)
-      return d
-    },
   },
 })

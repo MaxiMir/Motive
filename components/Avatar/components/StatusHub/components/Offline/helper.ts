4462b49c4cc57a73d272b11b0b1bcc32dca8a7ac
Max <30494646+MaxiMir@users.noreply.github.com>
2022-10-17 13:54:26 +0300

    feat(basic): creating pages (#11)
    Co-authored-by: Максим Минченко <minchenko_ma@nlmk.com>

--- components/Avatar/components/StatusHub/components/Offline/helper.ts
+++ components/Avatar/components/StatusHub/components/Offline/helper.ts
@@ -1,6 +1,6 @@
 export const getShortDistance = (distance: string): string => {
   const words = distance.split(' ')
-  const value = words.at(-2)
+  const value = words.at(-2)?.replace(/\D/g, '')
   const measurement = words.at(-1)?.[0] || ''
 
   return `${value} ${measurement}`

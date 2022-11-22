23ce74ccf3411fcece4e2e552b3ed0b94f0a8c05
Максим Минченко <minchenko_ma@nlmk.com>
2022-11-22 11:58:05 +0300

    feat(basic): update ui

--- src/common/components/Gallery/GalleryViewer.tsx
+++ src/common/components/Gallery/GalleryViewer.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import dynamic from 'next/dynamic'
+import { useIntl } from 'react-intl'
 import 'react-image-lightbox/style.css'
 import { PhotoDto } from '@dto'
 import { getImageSrc } from '@href'
@@ -12,21 +13,24 @@ export interface GalleryViewerProps {
 }
 
 export default function GalleryViewer({ photos }: GalleryViewerProps) {
+  const { formatMessage } = useIntl()
   const [index, setIndex] = useState<number>()
   const sources = photos.map(({ src }) => getImageSrc(src))
+  const nextLabel = formatMessage({ id: 'common.next' })
+  const prevLabel = formatMessage({ id: 'common.previous' })
   const open = typeof index === 'number'
   const count = sources.length
 
   const onCloseRequest = () => setIndex(undefined)
 
   const onMovePrevRequest = () => {
-    if (!index) return
+    if (!open) return
 
     setIndex((index + count - 1) % count)
   }
 
   const onMoveNextRequest = () => {
-    if (!index) return
+    if (!open) return
 
     setIndex((index + 1) % count)
   }
@@ -35,6 +39,8 @@ export default function GalleryViewer({ photos }: GalleryViewerProps) {
     <>
       {open && (
         <Lightbox
+          nextLabel={nextLabel}
+          prevLabel={prevLabel}
           mainSrc={sources[index]}
           nextSrc={sources[(index + 1) % count]}
           prevSrc={sources[(index + count - 1) % count]}

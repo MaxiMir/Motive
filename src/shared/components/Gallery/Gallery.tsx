a209f2fd94cb03396f0f7ca58f2afeaec1540183
Максим Минченко <minchenko_ma@nlmk.com>
2023-01-08 19:25:29 +0300

    feat(basic): update menu

--- src/shared/components/Gallery/Gallery.tsx
+++ src/shared/components/Gallery/Gallery.tsx
@@ -1,22 +1,27 @@
 import { useState } from 'react'
+import PhotoAlbum, { ClickHandlerProps } from 'react-photo-album'
 import { PhotoDto } from '@features/feedback'
 import AppLightBox from '@ui/AppLightBox'
-import GallerySimple from './GallerySimple'
+import GalleryPhoto from './components/GalleryPhoto'
 
 export interface GalleryViewerProps {
   photos: PhotoDto[]
 }
 
-function GalleryViewer({ photos }: GalleryViewerProps) {
+function Gallery({ photos }: GalleryViewerProps) {
   const [index, setIndex] = useState<number>()
   const sources = photos.map(({ src }) => src)
+
+  const onClick = (props: ClickHandlerProps) => {
+    setIndex(props.index)
+  }
 
   return (
     <>
       <AppLightBox sources={sources} index={index} setIndex={setIndex} />
-      <GallerySimple photos={photos} onClick={setIndex} />
+      <PhotoAlbum layout="rows" photos={photos} renderPhoto={GalleryPhoto} onClick={onClick} />
     </>
   )
 }
 
-export default GalleryViewer
+export default Gallery
---
+++ src/shared/components/Gallery/components/GalleryPhoto.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { RenderPhotoProps } from 'react-photo-album'
+import AppImage from '@ui/AppImage'
+
+function GalleryPhoto({
+  imageProps: { src, alt, title, sizes, className, onClick },
+  wrapperStyle,
+}: RenderPhotoProps) {
+  return (
+    <div style={wrapperStyle}>
+      <div style={{ display: 'block', position: 'relative', width: '100%', height: '100%' }}>
+        <AppImage
+          fill
+          src={src}
+          alt={alt}
+          title={title}
+          sizes={sizes}
+          className={className}
+          draggable={false}
+          onClick={onClick}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default GalleryPhoto

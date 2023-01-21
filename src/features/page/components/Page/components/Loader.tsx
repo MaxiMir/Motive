b2abe69b0ae08c3e3f9797c59222f4a76180bea1
Максим Минченко <minchenko_ma@nlmk.com>
2023-01-21 09:06:53 +0300

    feat(basic): refactor snackbar

--- src/features/page/components/Page/components/Loader.tsx
+++ src/features/page/components/Page/components/Loader.tsx
@@ -9,7 +9,7 @@ function Loader() {
       sx={{
         [`& .${alertClasses.root}`]: {
           aspectRatio: '1 / 1',
-          backgroundColor: 'rgba(0, 0, 0, .5)',
+          backgroundColor: 'rgba(0, 0, 0, .8)',
         },
         top: {
           xs: 80,

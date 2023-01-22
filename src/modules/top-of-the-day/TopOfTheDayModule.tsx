83f6fa53974f420221241c3cacfdb7fa9c1da1a2
Максим Минченко <minchenko_ma@nlmk.com>
2023-01-22 10:31:05 +0300

    feat(basic): add Stack

--- src/modules/top-of-the-day/TopOfTheDayModule.tsx
+++ src/modules/top-of-the-day/TopOfTheDayModule.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import AppContainer from '@ui/AppContainer'
 import AppFadeIcon from '@ui/AppFadeIcon'
 import { useMessages } from './hooks/useMessages'
@@ -11,12 +11,20 @@ function TopOfTheDayModule() {
       <Typography variant="h1" sx={{ mb: 3 }}>
         {messages.header}
       </Typography>
-      <Stack alignItems="center" spacing={2} flex={1} height="80dvh">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        flex={1}
+        gap={1}
+        height="80dvh"
+      >
         <Typography variant="h5" component="p" color="primary">
           {messages.text}
         </Typography>
         <AppFadeIcon name="keyboard" />
-      </Stack>
+      </Box>
     </AppContainer>
   )
 }

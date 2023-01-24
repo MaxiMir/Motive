5ea376057fd370a76164c0ec502a6a9aa9942e52
Максим Минченко <minchenko_ma@nlmk.com>
2023-01-24 19:42:27 +0300

    feat(basic): refactor modules

--- src/pages/user/components/EditProfile/components/EditModal/hooks/useUpdateUser.ts
+++ src/pages/user/components/EditProfile/components/EditModal/hooks/useUpdateUser.ts
@@ -3,10 +3,35 @@ import { useRouter } from 'next/router'
 import { useFormik } from 'formik'
 import { useIntl } from 'react-intl'
 import { useMutation } from 'react-query'
-import { useMutateUserPage, useUserContext } from '@views/user/hooks'
-import { UserPageDto } from '@modules/page'
-import { UpdateUserDto, UserBaseDto, UserService, toHref, profileSchema } from '@modules/user'
+import { mixed, object, string } from 'yup'
 import { getCurrentSearchParams, setSearchParams } from '@helpers/url'
+import { useMutateUserPage, useUserContext } from '@pages/user/hooks'
+import {
+  UserPageDto,
+  UpdateUserDto,
+  UserBaseDto,
+  toHref,
+  getUsers,
+  updateUser,
+} from '@entities/user'
+
+const validationSchema = object({
+  name: string()
+    .trim()
+    .required('The name is needed')
+    .min(3, "It's too short.")
+    .max(100, "It's so long."),
+  nickname: string()
+    .trim()
+    .required('A nickname is needed')
+    .min(3, "It's too short.")
+    .max(255, "It's so long.")
+    .matches(/^[a-z0-9\-_]+$/, 'Only lowercase letters, numbers and "-" and "_"'),
+  avatar: mixed().required('An avatar is needed'),
+  motto: string().trim().max(140, "It's so long."),
+  location: string().trim().min(3, "It's too short.").max(64, "It's so long."),
+  bio: string().trim().max(320, "It's so long."),
+})
 
 const getNextState = (page: UserPageDto, user: UserBaseDto) =>
   produce(page, (draft) => {
@@ -23,13 +48,13 @@ interface Options {
   data: UpdateUserDto
 }
 
-export const useForm = (onSuccess: () => void) => {
+export const useUpdateUser = (onSuccess: () => void) => {
   const { push } = useRouter()
   const user = useUserContext()
   const [page, mutatePage] = useMutateUserPage()
   const { formatMessage } = useIntl()
   const nicknameError = formatMessage({ id: 'page.user.modal-profile.nickname-error' })
-  const { mutateAsync } = useMutation(({ id, data }: Options) => UserService.update(id, data), {
+  const { mutateAsync } = useMutation(({ id, data }: Options) => updateUser(id, data), {
     onSuccess(dto) {
       const href = toHref(dto.nickname)
       const as = setSearchParams(href, getCurrentSearchParams())
@@ -48,12 +73,12 @@ export const useForm = (onSuccess: () => void) => {
       bio: user.bio,
       links: user.links,
     },
-    validationSchema: profileSchema,
+    validationSchema,
     async onSubmit(data, { setFieldError }) {
       const { id } = user
       const { nickname } = data
       const fetchParams = { where: { nickname }, page: 0, take: 1 }
-      const usersDB = user.nickname === nickname ? null : await UserService.get(fetchParams)
+      const usersDB = user.nickname === nickname ? null : await getUsers(fetchParams)
 
       if (usersDB?.length) {
         setFieldError('nickname', nicknameError)

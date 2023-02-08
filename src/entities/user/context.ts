4525ff3042d5cd478e680cadeb6c5bf7b05e197d
Max <30494646+MaxiMir@users.noreply.github.com>
2023-02-08 18:21:39 +0300

    feat(basic): refactor context (#47)
    Co-authored-by: Максим Минченко <minchenko_ma@nlmk.com>

--- src/entities/user/context.ts
+++ src/entities/user/context.ts
@@ -1,6 +1,6 @@
 import { createContext, useContext } from 'react'
 import { UserPageDto } from 'shared/api'
 
-export const UserContext = createContext<UserPageDto | null>(null)
+export const UserContext = createContext<UserPageDto>(null as never)
 
-export const useUserContext = () => useContext(UserContext) as UserPageDto
+export const useUserContext = () => useContext(UserContext)

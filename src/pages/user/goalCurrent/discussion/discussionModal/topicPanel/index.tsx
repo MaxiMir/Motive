8d272afe9e5f3c23f23c91f1af5e9b6c56305a2b
Максим Минченко <minchenko_ma@nlmk.com>
2023-02-19 08:52:47 +0300

    refactor(discussion): desktop hint

--- src/pages/user/goalCurrent/discussion/discussionModal/topicPanel/index.tsx
+++ src/pages/user/goalCurrent/discussion/discussionModal/topicPanel/index.tsx
@@ -1,9 +1,11 @@
-import { Stack, Typography } from '@mui/material'
+import { Stack } from '@mui/material'
 import { useIntl } from 'react-intl'
 import dynamic from 'next/dynamic'
 import CreateTopic from 'features/topic/create-topic'
 import { TopicType, UserBaseDto } from 'shared/api'
+import { useDeviceContext } from 'shared/ui/device'
 
+const Typography = dynamic(() => import('@mui/material/Typography'))
 const SupportSign = dynamic(() => import('entities/characteristic').then((m) => m.SupportSign))
 
 interface TopicPanelProps {
@@ -15,15 +17,18 @@ interface TopicPanelProps {
 
 function TopicPanel({ dayId, user, owner, clientGoal }: TopicPanelProps) {
   const { formatMessage } = useIntl()
+  const device = useDeviceContext()
   const enterText = formatMessage({ id: 'common.enter-to-send' })
   const type = !clientGoal ? TopicType.Question : TopicType.Support
   const withSupportSign = type === TopicType.Support && !clientGoal
 
   return (
     <Stack width="100%" pt={1} gap={1}>
-      <Typography variant="caption" sx={{ color: 'zen.silent', pl: 8 }}>
-        {enterText}
-      </Typography>
+      {device === 'desktop' && (
+        <Typography variant="caption" sx={{ color: 'zen.silent', pl: 8 }}>
+          {enterText}
+        </Typography>
+      )}
       <CreateTopic
         dayId={dayId}
         user={user}

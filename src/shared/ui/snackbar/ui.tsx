856590e095ca5ebc831833fe3dd17bb5a7fd845b
Максим Минченко <minchenko_ma@nlmk.com>
2023-03-11 18:10:19 +0300

    fix(snackbar): change color

--- src/shared/ui/snackbar/ui.tsx
+++ src/shared/ui/snackbar/ui.tsx
@@ -1,6 +1,6 @@
 import { Fade, Snackbar as MuiSnackbar, SnackbarProps as MuiSnackbarProps } from '@mui/material'
 import Alert, { alertClasses } from '@mui/material/Alert'
-import { teal } from '@mui/material/colors'
+import { red, teal } from '@mui/material/colors'
 import { SnackbarCloseReason } from '@mui/material/Snackbar/Snackbar'
 import { SyntheticEvent } from 'react'
 
@@ -33,6 +33,9 @@ function Snackbar({ severity, icon, message, onClose, ...props }: SnackbarProps)
         },
         [`& .${alertClasses.standardSuccess}`]: {
           backgroundColor: teal[900],
+        },
+        [`& .${alertClasses.standardError}`]: {
+          backgroundColor: red[900],
         },
         [`& .${alertClasses.action}`]: {
           alignItems: 'center',

7bc60b137fb6bfdcb56b996483cce60471d20a68
Максим Минченко <minchenko_ma@nlmk.com>
2023-03-30 15:59:42 +0300

    fix(snackbar): fix unnecessary render

--- src/shared/ui/snackbar/ui.tsx
+++ src/shared/ui/snackbar/ui.tsx
@@ -1,9 +1,11 @@
-import { Fade, Snackbar as MuiSnackbar } from '@mui/material'
 import Alert, { alertClasses } from '@mui/material/Alert'
 import { red, teal } from '@mui/material/colors'
 import { SnackbarCloseReason } from '@mui/material/Snackbar/Snackbar'
 import { SyntheticEvent } from 'react'
+import dynamic from 'next/dynamic'
 import { useSnackbarStore } from './model'
+
+const MuiSnackbar = dynamic(() => import('@mui/material/Snackbar'))
 
 function Snackbar() {
   const { open, alertProps, closeSnackbar } = useSnackbarStore()
@@ -15,36 +17,39 @@ function Snackbar() {
   }
 
   return (
-    <MuiSnackbar
-      open={open}
-      autoHideDuration={3000}
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-      sx={{
-        [`& .${alertClasses.root}`]: {
-          color: 'common.white',
-        },
-        [`& .${alertClasses.message}`]: {
-          lineHeight: '1.625rem',
-        },
-        [`& .${alertClasses.standardSuccess}`]: {
-          backgroundColor: teal[900],
-        },
-        [`& .${alertClasses.standardError}`]: {
-          backgroundColor: red[900],
-        },
-        [`& .${alertClasses.action}`]: {
-          alignItems: 'center',
-        },
-        bottom: {
-          xs: 80,
-          lg: 24,
-        },
-      }}
-      TransitionComponent={Fade}
-      onClose={handleClose}
-    >
-      <Alert {...alertProps} />
-    </MuiSnackbar>
+    <>
+      {open && (
+        <MuiSnackbar
+          open
+          autoHideDuration={3000}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+          sx={{
+            [`& .${alertClasses.root}`]: {
+              color: 'common.white',
+            },
+            [`& .${alertClasses.message}`]: {
+              lineHeight: '1.625rem',
+            },
+            [`& .${alertClasses.standardSuccess}`]: {
+              backgroundColor: teal[900],
+            },
+            [`& .${alertClasses.standardError}`]: {
+              backgroundColor: red[900],
+            },
+            [`& .${alertClasses.action}`]: {
+              alignItems: 'center',
+            },
+            bottom: {
+              xs: 80,
+              lg: 24,
+            },
+          }}
+          onClose={handleClose}
+        >
+          <Alert {...alertProps} />
+        </MuiSnackbar>
+      )}
+    </>
   )
 }
 

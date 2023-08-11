6a28f6c16660a9b436184f17b52f315c51520568
Максим Минченко <minchenko_ma@nlmk.com>
2023-08-11 15:06:06 +0300

    feat(sentry): move to env

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: process.env.GITHUB_ID || '',
+  dsn: process.env.SENRY_DSN || '',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,

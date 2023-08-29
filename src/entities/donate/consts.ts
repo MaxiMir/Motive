c0ec4723356875e60c0e84a79ce2868d78e8568f
Максим Минченко <minchenko_ma@nlmk.com>
2023-08-29 08:33:46 +0300

    feat(wallet): added usdt

--- src/entities/donate/consts.ts
+++ src/entities/donate/consts.ts
@@ -2,10 +2,12 @@
 import BTCSrc from 'public/images/svg/btc.svg'
 import ETHSrc from 'public/images/svg/eth.svg'
 import LTCSrc from 'public/images/svg/ltc.svg'
+import USDTSrc from 'public/images/svg/usdt.svg'
 /* eslint-disable import/no-internal-modules */
 
 export const MY_WALLETS = [
-  { name: 'BTC', wallet: '1AmJZzeVH6wkJZ6a1FojJbHD1im9UZBar7', src: BTCSrc },
+  { name: 'USDT', wallet: '0xc7a7b4c59999c27aaae8affcd9bd9eb1f4724806', src: USDTSrc },
   { name: 'ETH', wallet: '0xc7a7b4c59999c27aaae8affcd9bd9eb1f4724806', src: ETHSrc },
+  { name: 'BTC', wallet: '1AmJZzeVH6wkJZ6a1FojJbHD1im9UZBar7', src: BTCSrc },
   { name: 'LTC', wallet: 'LLspFKQebdgpq1YECXfjdkQKWTXZnmDBRA', src: LTCSrc },
 ]
